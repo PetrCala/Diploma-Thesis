7db3591413a6936a9d93722b157be761b336132d
PetrCala <61505008@fsv.cuni.cz>
2022-12-22 16:28:36 +0100

    Folder restructuring'

--- P&P (2018) analysis/P&P exploration_ecox project.R
+++ P&P (2018) analysis/P&P exploration_ecox project.R
@@ -1,121 +1,100 @@
 ########################################################
-##### Psacharopoulos & Patrinos (2018) exploration #####
+####### Advanced Econometrics 21/22 WS - project #######
 ########################################################
 
 ### Packages ###
 library(readxl)
 library(lmtest) #coeftest
-library(metafor) #rma - FE/RE
-library(plm) # FE/RE
+library(metafor) #FE/RE
+library(AER) #IV regression
+library(stargazer) #LaTeX output
 
-#Adjust this later
-library(readr)
-library(tidyverse)
-library(ggplot2)
-library(readxl)
-library(stats)
-library(DescTools)
-library(sandwich)
-library(multiwayvcov)
-library(bayesm)
-library(puniform)
-library(haven)
-library(meta)
-library(AER)
-library(BMS)
-library(corrplot)
-library(foreign)
-library(xtable)
-library(LowRankQP)
-library(foreign)
-library(multcomp)
+#If any of the packages above are not installed on your device,
+#simply install them with "install.packages('package_name')"
 
+###########################
+### Data pre-processing ###
+###########################
 
-### Data preparation ###
-#setwd("C:/Users/hso20/OneDrive/Plocha/IES/Diploma-Thesis/P&P (2018) analysis")
-setwd("C:/Users/AU451FE/IES/Diploma-Thesis/P&P (2018) analysis")
+setwd('wd_path') #The working directory should contain the source data set
+setwd("C:/Users/hso20/OneDrive/Plocha/IES/Diploma-Thesis/P&P (2018) analysis")
 
+#Read the data set and convert it to an R data frame
 data_raw <- read_excel('Data transformed for R.xlsx', sheet = 'Data')
-data <- data.frame(data_raw, stringsAsFactors = TRUE) #converting to a data frame
-#View(data)
+data <- data.frame(data_raw, stringsAsFactors = TRUE)
 
-
-### MP ###
-
-#Remove missing values
+#Remove missing values and include only relevant columns
 relevant_columns <- c('mp_overall', 'schooling_years', 'country',
                       'year', 'region', 'inc_level')
 mp_clean <- data[!is.na(data$mp_overall) & !is.na(data$schooling_years), 
                  relevant_columns]
+
+#Create a column 'obs' which denotes the number of occurrences in
+# the data set for each country 
+mp_clean$obs <- NA #Create an empty column
+countries <- table(mp_clean$country) #Get the information
+for (name in names(countries)) {
+  value <- as.numeric(countries[name]) #Number of occurrences
+  mp_clean[mp_clean$country == name,'obs'] <- value #Assign these
+}
+
+#Useful formulas
 gen_formula <- 'mp_overall ~ schooling_years'
+qdr_formula <- 'mp_overall ~ schooling_years + schooling_years^2'
 
+##################
+##### Models #####
+##################
 
-### Replication of methods from the primary study ###
+### Unweighted models ###
 
-#Figure 1 - rate of return ~ country's year of schooling
-#The basic regression should have coefficients 49.611 (intercept) and -0.020 (year)
-#Pretty much the same regression as in fig 2, only with data from Annex 1 - not sure if want to use
+#A simple OLS
+ols <- lm(formula = gen_formula, data = mp_clean)
+ols_c <- coeftest(ols, vcov = vcov(ols, cluster = c(mp_clean$country)))
 
-#Figure 2 - rate of return ~ years of schooling (individual observations)
-fig2_reg <- lm(formula = gen_formula, data = mp_clean)
-
-
-### New methodology ###
-
-#OLS where the standard errors are clustered on country level
-
-
-OLS_country_se <- lm(formula = gen_formula, data = mp_clean)
-OLS_country_se_c <- coeftest(OLS_country_se, vcov = vcovHC(OLS_country_se,
-                            type = "HC0", cluster = c(mp_clean$country)))
-print(OLS_country_se_c)
-
-
-
-#Try running the between/within methods possibly
-
-#within - fixed effects, random - randomeffects, between - between
-#Read on the "effect = "twoways""
-
-pdf <- pdata.frame(mp_clean, index = c("mp_overall", "schooling"))
-
-fe <- plm(formula = gen_formula, data = mp_clean, model = "within")
-fe_c <- coeftest(fe, vcov = vcov(fe, type = "fixed", cluster = c(mp_clean$country)))
-#table(index(your_pdataframe), useNA = "ifany")
-
-sum(is.na(mp_clean$mp_overall))
-
-
-re <- 0
-re_c <- coeftest(re, vcov = vcov(re, type = "fixed", cluster = c(mp_clean$country)))
-
-we <- 0
-we_c <- coeftest(we, vcov = vcov(we, type = "fixed", cluster = c(mp_clean$country)))
-##Bachelor thesis code
-
-
+#Simple Fixed-effects
 fe <- rma(mp_overall, sei = schooling_years, mods = ~schooling_years,
           data = mp_clean, method = "FE")
+fe_c <- coeftest(fe,
+                 vcov = vcov(fe, type = "fixed", cluster = c(mp_clean$country)))
+
+#A simple regression with the quadratic term
+nls <- nls(formula = qdr_formula, data = mp_clean,
+           start = list(schooling_years = 0))
+nls_c <- coeftest(nls, vcov = vcov(nls, cluster = c(mp_clean$country)))
+
+stargazer(ols_c, fe_c, nls_c) #LaTeX table for the simple models
 
 
-#FE
-FE <- rma(pcc_w, sei = se_pcc_w, mods = ~se_pcc_w, data = dataset, method = "FE")
-FE_c <- coeftest(FE, vcov = vcov(FE, type = "fixed", cluster = c(dataset$study_id))) 
-print(FE_c)
+### Weighted models ###
 
-#Between
-RE <- rma(pcc_w, sei = se_pcc_w, mods = ~se_pcc_w, data = dataset, method = "REML")
-RE_c <- coeftest(RE, vcov = vcov(RE, type = "fixed", cluster = c(dataset$study_id))) 
-print(RE_c)
+#Weighted OLS
+ols_w <- lm(formula = gen_formula, data = mp_clean,
+            weight = (mp_clean$obs*mp_clean$obs))
+ols_w_c <- coeftest(ols_w, vcov = vcov(ols_w, cluster = c(mp_clean$country)))
+
+#Weighted Fixed-effects
+fe_w <- rma(mp_overall, sei = schooling_years, mods = ~schooling_years,
+        weights = (mp_clean$obs*mp_clean$obs), data = mp_clean, method = "FE")
+fe_w_c <- coeftest(fe_w,
+          vcov = vcov(fe_w, type = "fixed", cluster = c(mp_clean$country)))
+
+#Weighted regression with the quadratic term
+nls_w <- nls(formula = qdr_formula, data = mp_clean,
+      start = list(schooling_years = 0), weight = (mp_clean$obs*mp_clean$obs))
+nls_w_c <- coeftest(nls_w, vcov = vcov(nls_w, cluster = c(mp_clean$country)))
+
+stargazer(ols_w_c, fe_w_c, nls_w_c) #LaTeX table for the weighted models
 
 
-#Quadratic term formula #reconsider procedure
-
-qdr_formula <- 'mp_overall ~ schooling_years + schooling_years^2'
-test <- gmm(g = qdr_formula, x = mp_clean)
-test
 
 
 
 
+### IV regression ###
 
+instrument <- 1/sqrt(mp_clean$obs) #instrument to be used as weight
+iv_formula <- 'mp_overall ~ schooling_years | instrument'
+IV_reg <- ivreg(formula = iv_formula, data = mp_clean)
+summary(IV_reg, vcov = vcov(IV_reg, cluster = c(mp_clean$country)),
+        diagnostics = TRUE) #Diagnostic tests included in the summary
---
+++ R/test.r
@@ -0,0 +1,4 @@
+﻿a <- 1
+hello_world <- NA
+print(a)
+
