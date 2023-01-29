98cac0fbce297b783943923e638b604355f4b6ae
PetrCala <61505008@fsv.cuni.cz>
2023-01-29 13:42:24 +0100

    Finalized the query data, ready for identification

---
+++ Literature/Literature helper/lithelp.py
@@ -0,0 +1,243 @@
+﻿#----- A python script to help preprocess literature information, and extract useful pieces of information,
+# such as the author, year of publication, etc.
+
+#### HOW TO: ####
+# 1. Place the list of study citations in the empty text file 'Source.txt'.
+# 2. Run the script
+# 3. A new file will be created, 'output.csv', where the information will be stored
+
+import regex as re
+
+import pandas as pd
+
+
+SOURCE_NAME = 'source'
+CITATION_LEN = 574 # Manually input the number of citations  you want to handle - check for validity of the file
+
+R_NAME = r'([\p{L}]+(?:\s[\p{L}]\.)?)' # Matches Newman L., or Newman, with any special characters included
+
+# Two authors
+A2_RGX = f'{R_NAME}, {R_NAME}, and {R_NAME} {R_NAME}'
+
+# Three or more authors
+A3_FIRST_AUTHOR = f'{R_NAME}, {R_NAME}, '
+A3_ADDITIONAL_AUTHOR = f'{R_NAME} {R_NAME}, '
+A3_LAST_AUTHOR = f'and {R_NAME} {R_NAME}'
+
+# Cite year regex
+PUB_YEAR_RGX_1 = r'\((\d{4})\)' # foo (2020) foo
+PUB_YEAR_RGX_2 = r'\s(\d{4})\.$' # foo 2020.end-of-line
+PUB_YEAR_RGX_3 = r', (\d{4}),' # foo, (2020), foo
+PUB_YEAR_RGXS = [PUB_YEAR_RGX_1, PUB_YEAR_RGX_2, PUB_YEAR_RGX_3]
+PUB_YEAR_MISSING_RGX = r'(\d{4})' # No year information in the citation
+
+
+def main():
+    doAll('titles_source', 'citations_source')
+
+
+def doAll(titles_file, citations_file):
+    '''Input the names of the two files and create a .csv file in the same folder with
+    all data neatly handled.
+    '''
+    print('Preprocessing all of the data...')
+    # Preprocess the citations
+    correct_citations = quoteCitations(titles_file, citations_file)
+
+    # Get various data
+    authors = extractAuthors(correct_citations)
+    years = extractYear(correct_citations)
+    labels = extractLabel(authors, years)
+    titles = readSource(titles_file)
+
+    # Create the pandas data frame
+    df = pd.DataFrame({'Label': labels,
+     'Author': authors, 
+     'Year': years, 
+     'Study title': titles,
+     'Citation': correct_citations,
+     })
+
+    # Create the .csv file
+    df = df.to_csv(sep = ';', header=None, index=False).strip('\n').split('\n')
+    df_string = '\n'.join(df)  # <= this is the string that you can use with md5
+    df_bytes = df_string.encode('utf8')  # <= this is bytes object to write the file
+    with open('data/output.txt', 'wb') as f:
+        f.write(df_bytes)
+    print('Created the output file in the folder \"data\"')
+    return df
+
+def extractAuthors(citations):
+    '''Return a list of authors of all the citations.
+    '''
+    authors = []
+    unhandled = []
+
+    for citation in citations:
+        match = re.search('^[^"]*', citation) # Extract raw author information
+        if not match: # Incorrect citation form
+            continue
+        raw_author_text = match.group()
+        author = raw_author_text.replace(". ","").replace(";",",").replace("\ufeff","").title()
+        # Single author case
+        if not " And " in author:
+            surname_match = re.search('^[^,]*', author)
+            if not surname_match: # Funky name format, handle later
+                continue
+            # More commas
+            surname = surname_match.group()
+            if author.count(',') > 1: # Can be "author, author, author", or "author, et al"
+                new_name = f'{surname} et al.'
+                authors.append(new_name)
+                continue
+            authors.append(surname) # Single author, all standard
+            continue
+        # Multiple author case
+        # Two authors
+        a2_match = re.search(A2_RGX, author)
+        if a2_match:
+            author1 = a2_match.group(1)
+            author2 = a2_match.group(4)
+            new_name = f'{author1} & {author2}'
+            authors.append(new_name)
+            continue
+        if author.count(',') == 2:
+            author_split = author.split()
+            author1 = author_split[0]
+            author2 = author_split[-1]
+            new_name = f'{author1} & {author2}'
+            authors.append(new_name)
+            continue
+        # Three or more authors
+        a3_iterator = 1
+        a3_found = False
+        while a3_iterator < 5:
+            a3_rgx = A3_FIRST_AUTHOR + a3_iterator*A3_ADDITIONAL_AUTHOR + A3_LAST_AUTHOR
+            a3_match = re.search(a3_rgx, author)
+            if a3_match:
+                author_name = a3_match.group(1)
+                new_name = f'{author_name} et al.'
+                authors.append(new_name)
+                a3_found = True # Mark author found
+                a3_iterator = 5 # Exit the iteration
+            a3_iterator += 1
+        if a3_found:
+            continue
+        if author.count(',') > 2:
+            surname_match = re.search('^[^,]*', author) 
+            if surname_match:
+                surname = surname_match.group()
+                new_name = f'{surname} et al.'
+                authors.append(new_name)
+                continue
+        # Unhandled
+        authors.append(author)
+
+
+    missing = len(citations) - len(authors)
+    if missing != 0:
+        print(f'Missing {missing} authors...')
+    return authors
+
+def extractYear(citations):
+    years = []
+    for citation in citations:
+        # Regular formats
+        for rgx in PUB_YEAR_RGXS:
+            year_match = re.search(rgx, citation)
+            if year_match:
+                year = year_match.group(1)
+                years.append(year)
+                break
+        # No match found - no break called
+        else:
+            # No year number in citation
+            any_year_in_citation = re.search(PUB_YEAR_MISSING_RGX, citation)
+            if not any_year_in_citation:
+                years.append(None)
+                continue
+            # Unhandled search pattern
+            years.append('X')
+
+    missing = years.count('X')
+    if missing != 0:
+        print(f'Missing {missing} years...')
+    return years
+
+def extractLabel(authors, years):
+    labels = []
+    if not len(authors) == len(years):
+        raise ValueError('Mising some observations...')
+    for author, year in zip(authors, years):
+        label = f'{author} ({year})'
+        labels.append(label)
+    return labels
+
+
+def quoteCitations(titles_file:str, citations_file:str, verbose = True):
+    '''Input the name of titles file, and the citations file, and quote the titles around
+    citations, which are not quoted yet.
+    Return the list of quoted citations.
+    '''
+    # Read the source files - automatically validates length
+    titles_source = readSource(titles_file)
+    citations_source = readSource(citations_file)
+
+    # Merge to a single list
+    source = list(zip(titles_source, citations_source))
+    quoted_list = []
+    unhandled = []
+
+    # Loop tools
+    rgx = r'"(.*)"' # Extract between double quotes # Alternative - r'"([^"]*)"'
+
+    print('Quoting all citations...')
+    # Main loop
+    for s in source:
+        title, fancy_citation = s[0], s[1] # Unpack
+        citation = fancy_citation.replace("‘", "'").replace("’", "'") # Replace fancy quotes
+        match = re.search(rgx, citation)
+        # Double quotes
+        if match:
+            searched_title = match.group(1)
+            # Everything in order
+            if title in searched_title:
+                quoted_list.append(citation) # Correct citation form 
+                continue
+            # A little off - several words etc.
+            a,b = searched_title.split(), title.split()
+            a_matching = [i in b for i in a] # Words in a match words in b
+            if sum(a_matching)/len(a_matching) > 0.7: # A very similar title
+                quoted_list.append(citation)
+                continue
+            # Unhandled case with double quotes
+            unhandled.append(citation) # Unhandled cases, unknown cause of misbehavior
+            continue
+        # No double quotes
+        match = re.search(re.escape(title), citation) # re.escape to avoid handle special characters
+        if match: # Citation contains the title
+            new_citation = re.sub(re.escape(title), f'"{title}"', citation)
+            new_citation = new_citation.replace("\".", ".\"") # Fix study title endings
+            quoted_list.append(new_citation)
+            continue
+        # Title not inside citation, or any other unhandled case
+        unhandled.append(citation)
+        
+    if not len(unhandled) == 0:
+        if verbose:
+            print(f'The citation list is still incomplete. {len(unhandled)} citations missing...')
+        return False
+    print('All citations quoted succesfully. :)')
+    return quoted_list
+
+def readSource(source_name):
+    with open(f'{source_name}.txt', "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    if not len(lines) == CITATION_LEN:
+        raise ValueError(f'The source file has an incorrect number of rows.\nExpected: {CITATION_LEN}\nGot: {len(lines)}')
+    lines = [item.replace('\n','') for item in lines] # Remove \n
+    return lines
+
+if __name__ == '__main__':
+    main()
+    
---
+++ Literature/Literature helper/seleniumtools.py
@@ -0,0 +1,180 @@
+﻿import os
+from os.path import exists
+import requests
+import time
+import re
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pynput.keyboard import Key
+
+import static as st
+from input import Input
+
+I = Input()
+
+def main():
+    pass
+
+#----- SELENIUM METHODS -----
+def openDriver():
+    '''Open the selenium driver.
+    '''
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--service-log-path=/dev/null')
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    ser = Service(executable_path=ChromeDriverManager().install())
+    return webdriver.Chrome(service=ser, options=chrome_options)
+
+def openWebsite(driver, link:str):
+    '''Input a selenium driver and a link and open a website with that driver.
+    In case of a timeout, return False, otherwise return True.
+    '''
+    current_url = driver.current_url
+    if not current_url == link:
+        driver.get(link)
+        try:
+            WebDriverWait(driver,10).until(EC.url_to_be(link))
+        except TimeoutException:
+            return False
+    return True
+
+def searchGoogleScholar(driver, search_string):
+    '''Search the google scholar for a specific string (citation).
+    '''
+    search_bar = driver.find_element(By.NAME, st.SEARCH_BAR_NAME) # Find the search bar element
+    search_bar.send_keys(search_string)
+    search_button = driver.find_element(By.NAME, st.SEARCH_BUTTON_NAME) # Search button
+    search_button.click() # Click the search button
+    return None
+
+def getStudiesBox(driver):
+    '''Return the element with all studies.
+    '''
+    try:
+        studies_box = driver.find_element(By.ID, st.STUDIES_BOX_ID) # All studies
+    except NoSuchElementException: # Captcha window open most probably
+        captcha_handled = handleCaptcha(driver)
+        if not captcha_handled:
+            return False
+        studies_box = driver.find_element(By.ID, st.STUDIES_BOX_ID) # Search again
+    return studies_box
+
+def openStudyForDownload(driver, desired_study_cite):
+    '''Check whether the Google Scholar search results yielded the desired
+    study on the first hit. If it is so, click the download button and return
+    True, else return False.
+    '''
+    rgx = r'"(.*)"' # Extract between double quotes # Alternative - r'"([^"]*)"'
+    match = re.search(rgx, desired_study_cite)
+    if match is None:
+        print('This study is not in the correct citation form.')
+        return False
+    searched_title = match.group(1) # Title of searched study
+    studies_box = getStudiesBox(driver) # All studies
+    study_box = getStudyBox(studies_box, searched_title) # Correct study
+    if not study_box:
+        return False
+    try: # Get the PDF button
+        link = study_box.find_element(By.XPATH, st.STUDY_BOX_PDF_BTN_XPATH)
+    except Exception as e: # Get the study title instead (no PDF button)
+        link = study_box.find_element(By.XPATH, st.STUDY_BOX_TITLE_XPATH) # Existence validated in getStudyBox
+    link.click()
+    WebDriverWait(driver,10).until(EC.url_changes)
+    return True
+
+def handleCaptcha(driver):
+    '''Handle the captcha window. If handled successfully, return True,
+    else return False.
+    '''
+    captcha_url = driver.current_url
+    WebDriverWait(driver,20).until(EC.presence_of_element_located((By.ID, st.STUDIES_BOX_ID)))
+    if captcha_url == driver.current_url:
+        print('Captcha failed.')
+        return False
+    return True
+
+def getStudyBox(studies_box, searched_title):
+    '''Insert the box with all studies on the page, iterate through the first 3
+    elements, and check, whether any matches the study title. If so, return
+    the correct study box element. Else return False.
+    '''
+    for i in range(1,4):
+        try:
+            study_box = studies_box.find_element(By.XPATH, f"div[{i}]") # Get a new study
+            link_el = study_box.find_element(By.XPATH, st.STUDY_BOX_TITLE_XPATH) # Get the title of that study
+            found_title = str(link_el.text)
+            searched_list, found_list = searched_title.split(), found_title.split()
+            matches = [(i in found_list) for i in searched_list]
+            if sum(matches)/len(searched_list) > 0.6:
+                print(f'{searched_title} found.')
+                return link_el
+        except Exception as e:
+            continue # Study not in this box, or only citation
+    print(f'{searched_title} not found.')
+    return False
+
+def getStudyCitation(driver, study_box):
+    '''Input a study box and open the cite window. Get the harvard citation text,
+    close the window, and return the text as a string, or None if it was not found.
+    '''
+    cite_el = study_box.find_element(By.CLASS_NAME, st.STUDY_BOX_CITE_CLASS_NAME) # Get cite el
+    cite_el.click()
+    try:
+        WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CLASS_NAME, st.CITE_CLASS)))
+        citation_div = driver.find_element(By.CLASS_NAME, st.CITE_CLASS)
+        print(citation_div.get_attribute('class'), citation_div.get_attribute('text'))
+        text = citation_div.text
+        # Close the window
+        I.useKey(Key.esc)
+        # WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, st.CITE_WINDOW_CLOSE_BUTTON_CLASS))).click()
+        return text
+    except Exception as e:
+        print(e)
+        return None
+
+def downloadStudy(driver, author):
+    '''After the study download page has been opened, download the study.
+    If the download is successful, return True. Otherwise return False.
+    '''
+    url_ = driver.current_url
+    if not 'pdf' in url_:
+        pdf_link = openPDFPage(driver) # Get the PDF page
+        if response is None:
+            return False
+        response = requests.get(pdf_link)
+    else:
+        response = requests.get(url_) # Already on PDF page
+    if not exists(st.download_path):
+        os.makedirs(st.download_path)
+    pdf_download_path = st.download_path + str(f'\{author}.pdf')
+    with open(pdf_download_path, "wb") as f:
+        f.write(response.content)
+    print(f'{author} downloaded.')
+    return True
+
+def openPDFPage(driver):
+    '''Check the type of the (potentially downloadable) website, which the driver is
+    currently on. Perform any necessary preparations/send inputs, until the page with
+    a downloadable PDF is open. If this succeeds, return the pdf link (response).
+    Otherwise return None.
+    '''
+    url_ = driver.current_url
+    if 'sciencedirect' in url_:
+        view_pdf_btn = driver.find_element(By.XPATH, st.SCIENCE_DIRECT_VIEW_PDF_XPATH)
+        view_pdf_btn.click()
+        WebDriverWait(driver,10).until(EC.url_changes)
+        download_pdf_btn = driver.find_element(By.XPATH, st.SCIENCE_DIRECT_PDF_BTN_XPATH)
+        download_link = download_pdf_btn.get_attribute('href')
+        return download_link
+    return False
+
+
+if __name__ == '__main__':
+    main()
+
