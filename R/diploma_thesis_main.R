8d315fc472d15708311e1edc3dd4c1725d4728d1
PetrCala <61505008@fsv.cuni.cz>
2023-02-23 10:10:55 +0100

    Added pathing to the main R script, automated several things

--- R/diploma_thesis_main.R
+++ R/diploma_thesis_main.R
@@ -1,30 +1,52 @@
 ##################### ENVIRONMENT PREPARATION ########################
 
+# Sys.setlocale("LC_ALL", "en_US.UTF-8") # Set the correct locale
+
 #Clean the environment
 rm(list = ls()) 
 
-# Load the source script
-source("diploma_thesis_source.R")
-
-# Static
-master_data_set_path = "../Data/data_set_master_thesis_cala.xlsx" # Master data set in folder Data
-
-
-####################### PACKAGE HANDLING ########################
+##### STATIC #####
+master_data_set_source <- "data_set_master_thesis_cala.csv" # Master data frame
+source_files <- c(
+  master_data_set_source
+)
 
 # Required packages
 packages <- c("readr", "tidyverse", "ggplot2", "readxl", "stats", "DescTools", "sandwich", "lmtest", "multiwayvcov",
               "metafor", "bayesm", "puniform", "haven", "meta", "AER", "BMS", "corrplot", "foreign", "xtable",
-              "LowRankQP", "foreign", "multcomp", "data.table")
+              "LowRankQP", "foreign", "multcomp", "data.table", "dplyr")
+
+##### PREPARATION #####
+
+# Load the source script
+if (!file.exists("diploma_thesis_source.R")){
+  print('Please make sure to put the source file \"diploma_thesis_source\" in
+        your working directory.')
+} else{
+  source("diploma_thesis_source.R")
+  print("Source file loaded.")
+}
 
 # Load packages
 loadPackages(packages)
 
+# Copy the master data frame from ./Data into the WD (DELETE IN PRODUCTION)
+master_data_set_xlsx_path = "../Data/data_set_master_thesis_cala.xlsx" # Master data set in folder Data
+copyMasterDF(xlsx_path = master_data_set_xlsx_path, csv_path = master_data_set_source)
+
+# Validate all the necessary files
+validateFiles(source_files)
 
 ######################### DATA PREPROCESSING #########################
 
 # Read the data set into the environment
-data_raw <- read_xlsx(master_data_set_path, sheet = 'main')
+data_raw <- read_csv(
+              master_data_set_source,
+              locale = locale(decimal_mark=".",
+                              grouping_mark=",",
+                              tz="UTC"),
+              show_col_types = FALSE) # Quiet warnings
+# data_raw <- read_xlsx(master_data_set_xlsx_path, sheet = 'main')
 
 # Data transformation
 data <- copy(data_raw) # Make a deep copy
@@ -33,15 +55,26 @@ data <- preprocessData(data) # Validate, preprocess, and winsorize data
 
 ######################### DATA EXPLORATION #########################
 
+###### SUMMARY STATISTICS ######
+
+summary_stats_desc <- loadSummaryStats()
+getSummaryStats(data, summary_stats_desc)
+
+###### FUNNEL PLOT ######
+
 # Filter out the outliers
 filter_pcc_w <- getOutliers(data, pcc_cutoff=0.8, precision_cutoff=0.1, verbose=T)
 filter_pcc_w <- getOutliers(data, pcc_cutoff=1, precision_cutoff=1, verbose=T) # Allow all
-funnel_data <- data[filter_pcc_w, c('pcc_w', 'se_precision_w')]
 
-# Funnel plot
+# Single out the data for the funnel plot
+funnel_data <- data[filter_pcc_w, c('pcc_w', 'se_precision_w')] # Only PCC, Precision
+funnel_data[] <- lapply(funnel_data, as.numeric) # To numeric
+
+# Plot the plot
 funnel_win <- ggplot(data = funnel_data, aes(x = pcc_w, y = se_precision_w)) + 
   geom_point(color = "#0d4ed1") + 
   labs(title = NULL, x = "Partial correlation coefficient", y = "Precision of the estimate (1/SE)") +
   main_theme()
   
 suppressWarnings(print(funnel_win)) # Print out the funnel plot
+
--- R/diploma_thesis_source.R
+++ R/diploma_thesis_source.R
@@ -1,3 +1,42 @@
+##################### ENVIRONMENT PREPARATION ########################
+
+#' DELETE IN PRODUCTION
+#' 
+#' Copy the existing excel data frame into a basic .csv file in
+#'  the working directory
+#'  
+#' :args:
+#'  xlsx_path [str] - Path to the main, already existing excel file.
+#'  csv_path [str]- Path to where the new .csv file should be created.
+copyMasterDF <- function(xlsx_path, csv_path){
+  data_raw <- read_xlsx(xlsx_path, sheet = 'main')
+  
+  # Remove .
+  data_raw[data_raw == '.'] <- NA
+  
+  write_csv(data_raw,
+            csv_path)
+  print(".csv data source file created in the working directory.")
+}
+
+
+#' Input a vector of file names, that should be located in the folder
+#' of the main script, and validate that all are indeed present.
+#' Print out a status message after the validation.
+#' 
+#' :args:
+#'  files[vector] - A vector of strings.
+validateFiles <- function(files){
+  for (file in files){
+    if (!file.exists(file)){
+      stop(paste0(file, ' does not exist or could not be located.
+                  Please make sure to include it in the working directory.'))
+    }
+  }
+  print("All necessary files located successfully.")
+}
+
+
 ####################### PACKAGE HANDLING ########################
 
 #' Package loading function
@@ -40,6 +79,9 @@ preprocessData <- function(input_data, win_level = 0.01){
     return(NA)
   }
   
+  # Conver double to numeric
+  #input_data <- input_data %>% mutate_all(~ ifelse(is.double(.), as.numeric(.), .))
+  
   # Get the winsorization interval
   if (!(0 < win_level & win_level <1)){
     stop('Incorrect winsorization level. Choose a float between 0 and 1.')
@@ -58,6 +100,97 @@ preprocessData <- function(input_data, win_level = 0.01){
 }
 
 ######################### DATA EXPLORATION #########################
+
+#' Load the identifiers of various summary stats that should be used
+#' during the data exploration.
+loadSummaryStats <- function(){
+  return(c(
+    "schooling_years" = 1,
+    "schooling_levels" = 1,
+    "data_type_micro" = 1,
+    "data_type_survey" = 1,
+    "data_type_national_register" = 1,
+    "data_cross_section" = 1,
+    "data_panel" = 1,
+    "gender_male1" = "gt0.5",
+    "gender_male2" = "lt0.5",
+    "sector_urban" = 1,
+    "sector_rural" = 1
+  ))
+}
+
+
+#' Input a data frame, and a vector of variables and their values for which to
+#' load the summary statistics, and return a data frame of these summary statistics.
+#' 
+#' :args:
+#'  data [data.frame] - The input data frame
+#'  sum_stats [vector] - A vector in the form of a dictionary, such as
+#'    c("var_name1" = "value1",
+#'    "var_name2" = "value2")
+#'    The values can also be prepended with "lt" (lower than) or
+#'    "gt" (greater than), such as "var1" = "gt0.5".
+#'  conf.level [float] - Confidence level for the confidence interval.
+#'    Defaults to 0.95
+#'  weight [bool] - If True, return weighted mean instead of a usual mean.
+#'    Defaults to FALSE.
+#'  
+#'  :return:
+#'    [data.frame] - A data frame with summary statistic for all the specified
+#'      variables. These summary statistics are "mean", "SD", "lower CI bound",
+#'      "upper CI bound", "number of observations"
+getSummaryStats <- function (input_data, sum_stats, conf.level = 0.95, weight = FALSE) {
+  stats_list <- c("Variable", "Mean", "SD",
+                    "CI lower", "CI upper", "Obs") # Columns of the output df
+  z <- qnorm((1 - conf.level)/2, lower.tail = FALSE) # Z value 
+  df_rows <- length(sum_stats)
+  df_cols <- length(stats_list)
+  df_matrix <- matrix(nrow=df_rows, ncol=df_cols) #Temporary matrix
+  
+  for (i in names(sum_stats)) {
+    name <- i
+    value <- sum_stats[name]
+    last_char <- substr(name, nchar(name), nchar(name))
+    if (last_char %in% (c(1,2,3,4,5))) {
+      name <- substring(name, 1, nchar(i)-1)
+    }
+    
+    # Get display names
+    if (grepl("lt", value, fixed=TRUE)) {
+      value <- substr(value, 3, nchar(value))
+      filter <- input_data[,name]<as.numeric(value)
+      disp_name <- paste(name, "<", as.character(value))
+    } else if (grepl("gt", value, fixed=TRUE)) {
+      value <- substr(value, 3, nchar(value))
+      filter <- input_data[,name]>as.numeric(value)
+      disp_name <- paste(name, ">", as.character(value))
+    } else {
+      filter <- input_data[,name]==as.numeric(value)
+      disp_name <- name
+    }
+    
+    # Mean
+    if (weight != TRUE) {
+      xbar <- mean(input_data$pcc_w[filter]) #Simple mean
+    } else {
+      xbar <- weighted.mean(input_data$pcc_w[filter], #Weighted mean
+                            w = c(input_data$study_size*input_data$study_size)[filter])
+    }
+    sdx <- sd(input_data$pcc_w[filter])   #SD
+    conf_l <- xbar - z * sdx        #Confidence interval lower bound
+    conf_u <- xbar + z * sdx        #Confidence interval upper bound
+    obs <- sum(filter)              #Number of observations
+    # Resulting vector
+    out <- c(disp_name, round(xbar, 3), round(sdx, 3),
+             round(conf_l, 3), round(conf_u, 3), obs)
+    row_idx <- match(i, names(sum_stats))
+    df_matrix[row_idx,] <- out
+  }
+  df <- data.frame(df_matrix) # Main output df
+  colnames(df) <- stats_list # Rename columns
+  return(df)
+}
+
 
 #' Identify outliers in the data, return the filter which can be used
 #'  to get the data without these outliers.
