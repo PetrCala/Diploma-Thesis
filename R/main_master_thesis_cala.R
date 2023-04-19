b6e0db37506c5024d3423f8cea2000549912e5ab
PetrCala <61505008@fsv.cuni.cz>
2023-04-19 12:11:42 +0200

    Added call notes and several remarks to the files

--- R/main_master_thesis_cala.R
+++ R/main_master_thesis_cala.R
@@ -104,16 +104,16 @@ var_list_source <- "var_list_master_thesis_cala.csv" # Variable information file
 run_this <- c(
   "variable_summary_stats" = T,
   "effect_summary_stats" = T,
-  "box_plot" = T,
-  "funnel_plot" = T,
-  "t_stat_histogram" = T,
-  "linear_tests" = T,
-  "nonlinear_tests" = T,
-  "exo_tests" = T,
-  "p_hacking_tests" = T,
-  "bma" = T,
-  "fma" = T, # Should be ran together with BMA
-  "best_practice_estimate" = T
+  "box_plot" = F,
+  "funnel_plot" = F,
+  "t_stat_histogram" = F,
+  "linear_tests" = F,
+  "nonlinear_tests" = F,
+  "exo_tests" = F,
+  "p_hacking_tests" = F,
+  "bma" = F,
+  "fma" = F, # Should be ran together with BMA
+  "best_practice_estimate" = F
 )
 
 #' ADJUSTABLE PARAMETERS
