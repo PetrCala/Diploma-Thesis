585d06782b033dcc4f7658f4843a43cf62f44cbf
PetrCala <61505008@fsv.cuni.cz>
2023-05-26 12:02:52 +0200

    Made the html boxplot work using a bit of dark magic

--- script_runner_master_thesis_cala.R
+++ script_runner_master_thesis_cala.R
@@ -22,19 +22,19 @@ if (!require('rstudioapi')) install.packages('rstudioapi'); library('rstudioapi'
 user_params <- list(
   # RUN THESE PARTS OF THE MAIN SCRIPT
   run_this = list(
-    "variable_summary_stats" = T,
-    "effect_summary_stats" = T,
+    "variable_summary_stats" = F,
+    "effect_summary_stats" = F,
     "box_plot" = T,
-    "funnel_plot" = T,
-    "t_stat_histogram" = T,
-    "linear_tests" = T,
-    "nonlinear_tests" = T,
-    "exo_tests" = T,
-    "p_hacking_tests" = T,
-    "bma" = T,
-    "fma" = T, # Executable only after running BMA
-    "ma_variables_description_table" = T, # Executable only after running BMA
-    "bpe" = T # Executable only after running BMA
+    "funnel_plot" = F,
+    "t_stat_histogram" = F,
+    "linear_tests" = F,
+    "nonlinear_tests" = F,
+    "exo_tests" = F,
+    "p_hacking_tests" = F,
+    "bma" = F,
+    "fma" = F, # Executable only after running BMA
+    "ma_variables_description_table" = F, # Executable only after running BMA
+    "bpe" = F # Executable only after running BMA
   ),
   
   # CUSTOMIZABLE FILE NAMES
@@ -186,7 +186,7 @@ user_params <- list(
   
   # CACHE HANDLING
   # I recommend you use caches only after you are certain the functions run correctly
-  use_cache = TRUE, # Store intermediate output in a cache in the /_cache/ folder.
+  use_cache = FALSE, # Store intermediate output in a cache in the /_cache/ folder.
   cache_age = 3600, # In seconds - an hour
   
   # Development information
