c4855d3d629587a655e39d59b3379d9d7c1f56b1
PetrCala <61505008@fsv.cuni.cz>
2023-07-05 16:20:15 +0200

    Moved the main console log file into a more visible place

--- Dist/main_master_thesis_cala.R
+++ Dist/main_master_thesis_cala.R
@@ -164,7 +164,7 @@ readExcelAndWriteCsv(source_data_path, source_sheets, csv_suffix, temp_data_fold
 validateFiles(all_source_files)
 
 # Save the console output to a log file in the results folder
-log_file_path <- paste0(folder_paths$numeric_results_folder, user_params$export_log_file)
+log_file_path <- paste0(folder_paths$all_results_folder, user_params$export_log_file)
 if (file.exists(log_file_path)){quiet(system(paste("rm", log_file_path)))} # Clean output file
 quiet(sink()) # Empty the sink
 sink(log_file_path, append = FALSE, split = TRUE) # Capture console output
