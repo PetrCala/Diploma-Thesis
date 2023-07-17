adcc691e0419ff7059bead98282f708ea93c13ca
PetrCala <61505008@fsv.cuni.cz>
2023-07-17 16:53:55 +0200

    Added a method for generating BPE summary statistics

---
+++ update_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd "C:/Users/hso20/OneDrive/Plocha/IES/Diploma-Thesis"
+
+# Call update_user_params.sh
+echo "Running update_user_params.sh..."
+./shell/update_user_params.sh
+
+# Call update_dist.sh
+echo "Running update_dist.sh"
+./shell/update_dist.sh
